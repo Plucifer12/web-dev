03eb653d88e3a03d0bc5a5c395600a8e8f68ba31
Plucifer12 <156513010+Plucifer12@users.noreply.github.com>
2024-01-16 16:39:09 +0530

    Add files via upload

---
+++ JS file/page-13/script.js
@@ -0,0 +1,27 @@
+console.log("EXERCISE-12")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// Array.from(boxes)
+
+// document.querySelectorAll(".box").forEach(e => {
+//     e.style.backgroundColor = "steelblue"
+// });
+
+// document.querySelector(".box").style.backgroundColor = "yellow";
+
+document.getElementById("yellowbox").style.backgroundColor = "yellow",
+document.getElementById("yellowbox").style.color = "steelblue"
+
+document.getElementById("greenbox").style.backgroundColor = "green",
+document.getElementById("greenbox").style.color = "brown"
+
+document.getElementById("redbox").style.backgroundColor = "red",
+document.getElementById("redbox").style.color = "aqua"
+
+document.getElementById("purplebox").style.backgroundColor = "purple",
+document.getElementById("purplebox").style.color = "yellowgreen"
+
+document.getElementById("bluebox").style.backgroundColor = "blue",
+document.getElementById("bluebox").style.color = "pink"
