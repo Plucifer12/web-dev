06ff3020f93e505964dad2028153ff22ec202042
Prateek <156513010+Plucifer12@users.noreply.github.com>
2024-07-02 18:59:37 +0530

    Delete DAY-72 Working with Files fs and path Modules directory

--- DAY-72 Working with Files fs and path Modules/aboutpath.js
+++
@@ -1,9 +0,0 @@
-import path from "path"
-
-let mypath = "C:\\Users\\suraj\\Desktop\\Web Dev\\Backend file\\page-3\\prateek.txt"
-console.log(path.extname(mypath))
-
-console.log(path.dirname(mypath))
-console.log(path.basename(mypath))
-
-console.log(path.join("C:/", "programs\\prateek.txt"))
--- DAY-72 Working with Files fs and path Modules/mainpromise.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs/promises"
-
-let a = await fs.readFile("prateek.txt")
-
-let b = await fs.appendFile("prateek.txt", "\n\n\n\nthis is amazing promise")
-console.log(a.toString(), b)
